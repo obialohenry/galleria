6b35c2acf47fce70f52a1c2aa2fc6eb4ab6e21df
sisu <obilaorchisomebi123@gmail.com>
2026-01-24 03:00:44 +0100

    refactor(camera state): remove 'isSnapping' property previously added, as that is already being handled by the Camera plugin

--- lib/model/local/camera_state.dart
+++ lib/model/local/camera_state.dart
@@ -3,15 +3,6 @@ import 'package:camera/camera.dart';
 class CameraState {
   final CameraController controller;
   final int cameraIndex;
-  bool isSnapping;
 
-  CameraState({required this.controller, required this.cameraIndex, this.isSnapping = false});
-
-  // CameraState withChanges({CameraController? controller, int? cameraIndex, bool? isSnapping}) {
-  //   return CameraState(
-  //     controller: controller ?? this.controller,
-  //     cameraIndex: cameraIndex ?? this.cameraIndex,
-  //     isSnapping: isSnapping ?? this.isSnapping,
-  //   );
-  // }
+  CameraState({required this.controller, required this.cameraIndex});
 }
